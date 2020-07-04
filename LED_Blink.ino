f6f205e67e65399a2198bb7205b3460d555e2aad
Devon Riter <63297761+devonriter@users.noreply.github.com>
2020-07-04 11:33:02 -0500

    Update LED_Blink.ino

--- LED_Blink.ino
+++ LED_Blink.ino
@@ -1,23 +1,23 @@
 /*
-  Blink
+  LED_Blink
 
   Turns an LED on for one second, then off for one second, repeatedly.
 
   This example code is in the public domain.
 
-  http://www.arduino.cc/en/Tutorial/Blink
+  Video Tutorial: https://www.youtube.com/watch?v=Mwmak4vMJ0k&t=581s
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin 7 as an output.
   pinMode(7, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
+  delay(1000);                       // wait for a second
   digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+  delay(1000);                       // wait for a second
 }
